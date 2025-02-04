66a07441d42cb9c5a1afda3ead00766a644a2e7d
DHRUV <dhruv.globiz@gmail.com>
2025-02-04 17:27:56 +0530

    f

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/RvkqAXRHRkKlYZYi/scene.splinecode" 
+      />
+      
+       <Spline
+        scene="https://prod.spline.design/eZdxh5NZTlPjH42j/scene.splinecode" />
+        <Spline
+        scene="https://prod.spline.design/l2jvRPduMd57pJxy/scene.splinecode" 
+      />
+       <Spline
+        scene="https://prod.spline.design/5HglFBIbB1Fvo3x5/scene.splinecode" 
+      />
+    </main>
+  );
+}
